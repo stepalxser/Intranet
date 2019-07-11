690e2cb5279a5fa1ef375691e18970cfd8492316
Alexander Stepanov <stepalxser@gmail.com>
2019-07-11 13:19:59 +0300

    Structure page

--- webapp/main_page/models.py
+++ webapp/main_page/models.py
@@ -2,7 +2,7 @@ from webapp.database import db
 
 
 class Structure(db.Model):
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     lead = db.Column(db.String, db.ForeignKey('user.username'), nullable=False)
     parent_id = db.Column(db.Integer, nullable=False, default=0)
--- webapp/main_page/views.py
+++ webapp/main_page/views.py
@@ -24,3 +24,22 @@ def structure():
     primary_units = db.session.query(Structure.id, Structure.name, Structure.parent_id).all()
 
     return render_template('main_page/structure.html', page_title=title, primary_units=primary_units)
+
+
+@login_required
+@blueprint.route('/structure/<unit_id>')
+def structure_init(unit_id):
+    unit = Structure.query.filter_by(id=unit_id).first_or_404()
+    subunits = Structure.query.filter_by(parent_id=unit.id).all()
+
+    current_unit = Structure.query.filter_by(id=unit_id).first()
+    unit_links = []
+    while True:
+        unit_links.append((current_unit.id, current_unit.name))
+        if current_unit.parent_id != 0:
+            current_unit = Structure.query.filter_by(id=current_unit.parent_id).first()
+        else:
+            unit_links.reverse()
+            break
+
+    return render_template('main_page/structure_unit.html', unit_id=unit, subunits=subunits, unit_links=unit_links)
